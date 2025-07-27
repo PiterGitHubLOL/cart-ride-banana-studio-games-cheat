b168b77ee96675fcc3219cf208d3bd8edf9eb9f8
Piter Pitxr <piterglowne@gmail.com>
2025-07-27 18:22:54 +0200

    main script just run fling command on iy

---
+++ main.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PiterGitHubLOL/cart-ride-banana-studio-games-cheat/refs/heads/main/deleteseats"))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PiterGitHubLOL/cart-ride-banana-studio-games-cheat/refs/heads/main/tptobase"))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
